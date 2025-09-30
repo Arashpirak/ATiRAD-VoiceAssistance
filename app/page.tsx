01f06fbd085bcd175dc039d40bcb90a4a0cf143e
Arash <arashpirak@gmail.com>
2025-09-30 21:03:02 +0330

    refactor: replace VoiceAssistantWidget with placeholder content in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import VoiceAssistantWidget from "@/components/voice-assistant-widget";
-
-export default function Home() {
-  return <VoiceAssistantWidget />;
+export default function Page() {
+  return <div>{/* Your new widget or content */}</div>;
 }
